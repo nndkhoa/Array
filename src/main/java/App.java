9aa725d7759b540b51ee4b618adebce44bf78ef3
Khoa Ngô <nndkhoa9@gmail.com>
2021-08-11 08:56:08 +0700

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,29 @@
+public class App {
+  public static void main(String[] args) {
+    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    System.out.println(arr[3]);
+
+    // for (int i = 0; i < arr.length; i++) {
+    //   System.out.format("%d, ", arr[i]);
+    // }
+    // System.out.println();
+
+    for (int element : arr) {
+      System.out.format("%d, ", element);
+    }
+    System.out.println();
+
+    for (int i = arr.length - 1; i >= 0; i--) {
+      System.out.format("%d, ", arr[i]);
+    }
+    System.out.println();
+
+    // int j = 0;
+    // while (j < arr.length) {
+    //   System.out.format("%d, ", arr[j]);
+    //   j++;
+    // }
+    // System.out.println();
+  }
+}
