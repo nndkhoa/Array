7dfce74223ba9fe0165c5c95338783b86bbd9cf5
Khoa Ngô <nndkhoa9@gmail.com>
2021-08-11 08:58:55 +0700

    comment out all loc

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,22 @@
 public class App {
   public static void main(String[] args) {
-    int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-    System.out.println(arr[3]);
+    // int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    // System.out.println(arr[3]);
 
     // for (int i = 0; i < arr.length; i++) {
     //   System.out.format("%d, ", arr[i]);
     // }
     // System.out.println();
 
-    for (int element : arr) {
-      System.out.format("%d, ", element);
-    }
-    System.out.println();
+    // for (int element : arr) {
+    //   System.out.format("%d, ", element);
+    // }
+    // System.out.println();
 
-    for (int i = arr.length - 1; i >= 0; i--) {
-      System.out.format("%d, ", arr[i]);
-    }
-    System.out.println();
+    // for (int i = arr.length - 1; i >= 0; i--) {
+    //   System.out.format("%d, ", arr[i]);
+    // }
+    // System.out.println();
 
     // int j = 0;
     // while (j < arr.length) {
